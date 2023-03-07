19f737fc511941bf109dfed1518fe0851df01dcd
mukeshsin <110765718+mukeshsin@users.noreply.github.com>
2023-03-07 15:07:35 +0530

    signup page

--- polling-api/src/views/signup.vue
+++ polling-api/src/views/signup.vue
@@ -4,29 +4,29 @@
         <h3 class="head">Signup Form</h3>
         <p class="headPara">Please enter details to create a new user</p>
         <form @submit.prevent="formSubmit">
-            <label class="formLabel">Firstname:</label>
+            <label class="formLabel">Firstname</label>
             <input type="text" v-model="signupData.firstName" />
             <div v-if="firstNameValidate" class="error">{{ firstNameValidate }}</div>
-            <div v-if="!signupData.firstName" class="formLabel error">FirstName must be contain 4 character</div>
+            <div v-if="signupData.firstName" class="formLabel error">FirstName must be contain 4 character</div>
 
-            <label class="formLabel">Lastname:</label>
+            <label class="formLabel">Lastname</label>
             <input type="text" v-model="signupData.lastName" />
             <div v-if="lastNameValidate" class="error">{{ lastNameValidate }}</div>
-            <div v-if="!signupData.lastName" class="formLabel error">LastName must be contain 4 character</div>
+            <div v-if="!signupData.lastName " class="formLabel error">LastName must be contain 4 character</div>
 
-            <label class="formLabel">Password:</label>
+            <label class="formLabel">Password</label>
             <input type="password" v-model="signupData.password" />
-            <div v-if="!signupData.password" class="formLabel error">Password must be contain 8 character</div>
+            <div v-if="!signupData.password " class="formLabel error">Password must be contain 8 character</div>
 
-            <label class="formLabel">Email:</label>
+            <label class="formLabel">Email</label>
             <input type="email" v-model="signupData.email" />
-            <div v-if="!signupData.email" class="formLabel error">Please enter your email</div>
-            <div v-if="emailValidate" class="error">{{ emailValidate }}</div>
+            <div v-if="!signupData.email " class="formLabel error">Please enter your email</div>
+            <div v-if="emailValidate " class="error">{{ emailValidate }}</div>
 
-            <label class="formLabel">Role:</label>
+            <label class="formLabel">Role</label>
             <input type="role" v-model="signupData.roleId" />
             <div v-if="roleIdValidate" class="error">{{ roleIdValidate }}</div>
-            <div v-if="!signupData.roleId" class="formLabel error">RoleId cannot be empty</div>
+            <div v-if="!signupData.roleId " class="formLabel error">RoleId cannot be empty</div>
 
             <div class="formCheck">
                 <input type="checkbox" v-model="signupData.term" />
